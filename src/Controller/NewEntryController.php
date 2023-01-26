5e41191f9b5db8b3788377c516a81972be3b4435
AagieKLZ <darkyelan@gmail.com>
2023-01-26 14:00:05 +0100

    New page interface and form

--- src/Controller/NewEntryController.php
+++ src/Controller/NewEntryController.php
@@ -3,16 +3,36 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\HttpFoundation\Request;
 
 class NewEntryController extends AbstractController
 {
-    /**
-     * @Route('/new', name='new_entry')
-     */
-    public function newEntry()
+    public function createNewForm(Request $request)
     {
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, ['label' => 'Nombre'])
+            ->add('email', EmailType::class, ['label' => 'Correo Electrónico'])
+            ->add('tlf', TelType::class, ['label' => 'Teléfono'])
+            ->add('type', ChoiceType::class, ['choices' => ['Hotel' => 'Hotel', 'Pista' => 'Pista', 'Complemento' => 'Complemento'], 'label' => 'Categoría'])
+            ->add('active', CheckboxType::class, ['label' => 'Proveedor activo'])
+            ->add('submit', SubmitType::class, ['label' => 'Añadir'])
+            ->getForm();
 
-        return $this->render('default/new.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // do something with the data
+            $data = $form->getData();
+            // ...
+        }
+
+        return $this->render('default/new.html.twig', ['form' => $form->createView()]);
     }
 }
