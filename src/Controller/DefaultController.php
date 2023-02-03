681c129bad9f48b855afca51ce09c21676f71fe6
AagieKLZ <darkyelan@gmail.com>
2023-02-03 07:58:42 +0000

    Fixing DB connection

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EntryRepository;
 
 class DefaultController extends AbstractController
 {
