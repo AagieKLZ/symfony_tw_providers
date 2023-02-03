7230b747a53ff28f6277cee1c091471c1c1e702f
AagieKLZ <darkyelan@gmail.com>
2023-02-03 07:19:59 +0000

    Added: doctrine

--- src/DatabaseConnection.php
+++ src/DatabaseConnection.php
@@ -1,90 +1,51 @@
 <?php
 
-use Symfony\Component\Dotenv\Dotenv;
-use mysqli;
-use App\Entity\Entry;
+uuse App\Repository\EntryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DatabaseConnection
 {
-    private $connection;
+    private $entryRepository;
+    private $entityManager;
 
-    public function __construct()
+    public function __construct(EntryRepository $entryRepository, EntityManagerInterface $entityManager)
     {
-        (new Dotenv())->load(__DIR__.'/../.env');
-        $host = $_ENV['DATABASE_HOST'];
-        $username = $_ENV['DATABASE_USER'];
-        $password = $_ENV['DATABASE_PASSWORD'];
-        $dbname = $_ENV['DATABASE_NAME'];
-
-        $this->connection = new mysqli($host, $username, $password, $dbname);
-
-        if ($this->connection->connect_error) {
-            throw new Exception("Connection failed: " . $this->connection->connect_error);
-        }
+        $this->entryRepository = $entryRepository;
+        $this->entityManager = $entityManager;
     }
 
     public function getEntries($page)
     {
-        $sql = "SELECT * FROM Providers LIMIT 10 OFFSET ".(($page - 1) * 10);
-        $result = $this->connection->query($sql);
-        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $this->entryRepository->findBy([], ['id' => 'ASC'], 10, ($page - 1) * 10);
     }
+}
 
-    public function getPages()
-    {
-        #Get number of entries
-        $sql = "SELECT COUNT(*) FROM Providers";
-        $result = $this->connection->query($sql);
-        $n = mysqli_fetch_assoc($result)["COUNT(*)"];
-        return ceil($n / 10);
-    }
+public function getPages()
+{
+    $entryCount = $this->entryRepository->count([]);
+    return ceil($entryCount / 10);
+}
 
-    public function getEntry($id)
-    {
-        $sql = "SELECT * FROM Providers WHERE id = ?";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return mysqli_fetch_assoc($result);
-    }
+public function getEntry($id)
+{
+    return $this->entryRepository->find($id);
+}
 
-    public function addEntry(Entry $entry)
-    {
-        $name = $entry->getName();
-        $email = $entry->getEmail();
-        $tlf = $entry->getTlf();
-        $cat = $entry->getCat();
-        $active = $entry->getActive() ? 1 : 0;
-        $sql = "INSERT INTO Providers (name, email, tlf, cat, is_active, created_at, last_modified) VALUES (?, ?, ?, ?, ?, NOW(), NOW())";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("ssisi", $name, $email, $tlf, $cat, $active);
-        $stmt->execute();
-    }
+public function addEntry(Entry $entry)
+{
+    $this->entityManager->persist($entry);
+    $this->entityManager->flush();
+}
 
-    public function updateEntry($id, Entry $entry)
-    {
-        $name = $entry->getName();
-        $email = $entry->getEmail();
-        $tlf = $entry->getTlf();
-        $cat = $entry->getCat();
-        $is_active = $entry->getActive() ? 1 : 0;
-        $sql = "UPDATE Providers SET name = ?, email = ?, tlf = ?, cat = ?, is_active = ?, last_modified = NOW() WHERE id = ?";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("sssssi", $name, $email, $tlf, $cat, $is_active, $id);
-        $stmt->execute();
-    }
+public function updateEntry($id, Entry $entry)
+{
+    $this->entityManager->flush();
+}
 
-    public function deleteEntry($id)
-    {
-        $sql = "DELETE FROM Providers WHERE id = ?";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-    }
-
-    public function getConnection()
-    {
-        return $this->connection;
-    }
+public function deleteEntry($id)
+{
+    $entry = $this->entryRepository->find($id);
+    $this->entityManager->remove($entry);
+    $this->Entitymanager->flush();
+}
 }
