c5c1d6bdd0a1c08ab2cdc806762dccb85a5b0b1d
AagieKLZ <darkyelan@gmail.com>
2023-02-03 07:50:54 +0000

    Fixed syntax

--- src/DatabaseConnection.php
+++ src/DatabaseConnection.php
@@ -18,34 +18,33 @@ class DatabaseConnection
     {
         return $this->entryRepository->findBy([], ['id' => 'ASC'], 10, ($page - 1) * 10);
     }
-}
 
-public function getPages()
-{
-    $entryCount = $this->entryRepository->count([]);
-    return ceil($entryCount / 10);
-}
+    public function getPages()
+    {
+        $entryCount = $this->entryRepository->count([]);
+        return ceil($entryCount / 10);
+    }
 
-public function getEntry($id)
-{
-    return $this->entryRepository->find($id);
-}
+    public function getEntry($id)
+    {
+        return $this->entryRepository->find($id);
+    }
 
-public function addEntry(Entry $entry)
-{
-    $this->entityManager->persist($entry);
-    $this->entityManager->flush();
-}
+    public function addEntry(Entry $entry)
+    {
+        $this->entityManager->persist($entry);
+        $this->entityManager->flush();
+    }
 
-public function updateEntry($id, Entry $entry)
-{
-    $this->entityManager->flush();
-}
+    public function updateEntry($id, Entry $entry)
+    {
+        $this->entityManager->flush();
+    }
 
-public function deleteEntry($id)
-{
-    $entry = $this->entryRepository->find($id);
-    $this->entityManager->remove($entry);
-    $this->Entitymanager->flush();
-}
+    public function deleteEntry($id)
+    {
+        $entry = $this->entryRepository->find($id);
+        $this->entityManager->remove($entry);
+        $this->Entitymanager->flush();
+    }
 }
